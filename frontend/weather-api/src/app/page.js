b8d7c4fc66a6aa85586921f65c7b8006a8c2cad3
RobelGeb <robelgeb@uw.edu>
2024-09-23 02:14:18 -0700

    icon fix

--- frontend/weather-api/src/app/page.js
+++ frontend/weather-api/src/app/page.js
@@ -25,11 +25,11 @@ export default function Home() {
   }
 
   const getDay = (dateStr) => {
-    console.log(dateStr);
+    //console.log(dateStr);
     const yr = dateStr.substring(0, 4);
     const mo = dateStr.substring(5, 7);
     const day = dateStr.substring(8, 10);
-    console.log(`day: ${day}`);
+    //console.log(`day: ${day}`);
     var date = new Date(`${yr}-${mo}-${day}`);
     return date.toLocaleDateString(
       'en-US', 
@@ -40,6 +40,7 @@ export default function Home() {
   }
   
   const getIcon = (conditionStr) => {
+    console.log(conditionStr);
     if (conditionStr.includes("rain")) {
       return "rain.svg";
     } else if (conditionStr.includes("clear")) {
@@ -54,11 +55,11 @@ export default function Home() {
   return (
     <div className="flex flex-col h-full w-full bg-gradient-to-r from-slate-500 to-amber-950"> 
       <h1 className="flex mt-4 justify-center text-white text-3xl">Get your 5-day forecast</h1>       
-      <h2 className="flex justify-center mt-4 text-white text-3xl">
+      <h2 className="flex justify-center mt-4 text-white text-2xl">
         <form action={connect}>
           Enter City:
-          <input type="text" name="city" className="ml-3 bg-black/10 border-slate-800/10 border-2 backdrop-blur-[400px] hover:bg-black/30" />
-          <button type="submit" className="text-white px-2 mx-2 rounded-sm hover:bg-slate-500/35">Search</button>
+          <input type="text" name="city" className="ml-3 bg-black/10 border-slate-800/10 border-2 backdrop-blur-[400px] hover:bg-black/30"/>
+          <button type="submit" className="text-white px-2 mx-2 rounded-sm hover:bg-slate-900/30">Search</button>
         </form>
       </h2>
       {
@@ -66,15 +67,19 @@ export default function Home() {
         <div className="flex flex-row flex-direction justify-around h-full w-full">
           <div className="flex flex-col justify-center items-center w-2/5">
               <ul 
-                className="flex flex-col w-1/2 text-white bg-slate-100/20 backdrop-blur-[20px] p-6 my-10 rounded-md text-2xl" 
+                className="flex flex-col w-3/5 text-white backdrop-blur-[20px] p-6 my-10 rounded-md bg-slate-950/5 hover:bg-slate-100/5" 
                 key={todayData.datetime}
               >
-                <li className="mb-6 text-5xl">{getDay(todayData.datetime || '')}
-                </li>
-                <li className="mb-6 text-8xl">{todayData.temp} °F</li>
+                <li className="mb-6 text-7xl">Today</li>
+                <li className="mb-6 text-9xl">{todayData.temp} °F</li>
                 <div className="flex flex-row items-center">
-                  <li className="text-4xl">{todayData.conditions}</li>
-                  <Image src={require(`./icons/${getIcon(todayData.icons || '')}`)} width={100} height={100}></Image>
+                  <li className="text-5xl">{todayData.conditions}</li>
+                  <Image 
+                    src={require(`./icons/${getIcon(todayData.icon || '')}`)} 
+                    width={100} 
+                    height={100}
+                    alt={todayData.icon}
+                    ></Image>
                 </div>
                 
               </ul>        
@@ -84,13 +89,19 @@ export default function Home() {
               fiveDayData?.map((weather) => {
                 return(
                   <ul 
-                    className="flex items-start justify-center flex-col m-10 px-12 py-2 rounded-lg text-4xl text-white backdrop-blur-[400px]" 
+                    className="flex items-start justify-center flex-col m-10 px-12 py-2 rounded-lg text-4xl text-white backdrop-blur-[400px] hover:bg-slate-100/5" 
                     key={weather.datetime}
                   >
                     <li className="mb-6">{getDay(weather.datetime)}</li>
                     <li className="mb-6 text-6xl">{weather.temp} °F</li>
                     <li>{weather.conditions}</li>
-                    <Image src={require(`./icons/${getIcon(todayData.icons || '')}`)} width={100} height={100}></Image>
+                    <Image 
+                      src={require(`./icons/${getIcon(weather.icon || '')}`)} 
+                      width={100} 
+                      height={100}
+                      alt={weather.icon}
+                      >
+                    </Image>
                   </ul>          
                 );
               })
