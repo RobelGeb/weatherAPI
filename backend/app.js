1e3fbcafde4402ab45ad42a97f8262447deb3ce5
RobelGeb <r.gebremichael@outlook.com>
2024-09-30 15:36:21 -0700

    installed redis (pre-implement)

--- backend/app.js
+++ backend/app.js
@@ -3,32 +3,39 @@ const app =  express();
 const cors = require("cors");
 const superagent = require("superagent");
 const dotenv = require("dotenv");
-const rateLimit = require('express-rate-limit');
+const rateLimit = require("express-rate-limit");
+const Redis = require("redis");
 
-// const limiter = rateLimit({
-// 	windowMs: 15 * 60 * 1000, // 15 minutes
-// 	limit: 30, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-// 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-// 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-// })
+dotenv.config({path:"./config.env"});
+
+const client = Redis.createClient(process.env.REDIS_PORT);
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 30, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
 
 //app.use(limiter);
+
 app.use(
     cors({
         origin: '*',
     })
 );
+
 app.use(express.json({ extended: false }));
 
-dotenv.config({path:"./config.env"});
+client.on('error', err => console.log('Redis Client Error', err));
 
 const getWeather = async (city) => {
-    console.log(city.body);
+    console.log("city: ", city.body)
     try {
-        const weatherLink = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city.body.data}?unitGroup=us&elements=datetime%2Cname%2Ctemp%2Cfeelslike%2Cconditions%2Cdescription%2Cicon&include=days&key=57LJX6N358BY9PX8H6TY5S7DN&contentType=json`
+        const weatherLink = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city.body.data}?unitGroup=us&elements=datetime%2Cname%2Ctemp%2Cfeelslike%2Cconditions%2Cdescription%2Cicon&include=days&key=${process.env.API_KEY}&contentType=json`
         const data = await superagent.get(weatherLink);
-        //console.log(data.body);
         const report = data.body.days;
+        console.log("Resolved Address: ", data.body.resolvedAddress);
         return report;
     } catch (err) {
         console.log(err);
@@ -36,7 +43,7 @@ const getWeather = async (city) => {
 }
 
 app.post('/5day', async (req, res) => {
-    console.log(`request = ${JSON.stringify(req.body.data)}`);
+    //console.log(`request = ${JSON.stringify(req.body.data)}`);
     const result = await getWeather(req);
     const fiveDay = result.splice(0,5);
     res
